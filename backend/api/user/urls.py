aeeb44e1c067016d1fb4d01a01f5a6ff381bbff5
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-04-26 16:58:13 +0200

    register router for UserViewset

--- backend/api/user/urls.py
+++ backend/api/user/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from api.user import views as user_views
 
 
+router = DefaultRouter()
+router.register(r"user", user_views.UserViewSet, basename="user")
+
 urlpatterns = [
+    path("", include(router.urls)),
     path("login/", user_views.LoginView.as_view(), name="login"),
     path("logout/", user_views.LogoutView.as_view(), name="logout"),
 ]
