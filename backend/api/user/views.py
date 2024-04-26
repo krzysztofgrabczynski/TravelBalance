b308f15e04d620b45bfab1ed613c55340dce373d
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-04-26 23:45:41 +0200

    Update LoginView that will check if user is authenticated and active

--- backend/api/user/views.py
+++ backend/api/user/views.py
@@ -5,39 +5,94 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.http import HttpRequest
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 
 from api.user import serializers
 
 
-class LoginView(generics.CreateAPIView):
+class LoginView(generics.GenericAPIView):
     serializer_class = serializers.LoginSerializer
-    error_message = "Invalid credentials"
+    invalid_credentials_message = "Invalid credentials"
+    user_inactive_message = "User is not active"
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = authenticate(**serializer.validated_data)
 
-        if user:
-            token = self._create_user_auth_token(user)
-            return Response({"token": token.key, "status": status.HTTP_200_OK})
+        username = serializer.validated_data["username"]
+        password = serializer.validated_data["password"]
 
-        return Response(
-            {
-                "error": self.error_message,
-                "status": status.HTTP_401_UNAUTHORIZED,
-            }
-        )
+        # user = authenticate(**serializer.validated_data)
+
+        # if user:
+        #     token = self._create_user_auth_token(user)
+        #     return Response({"token": token.key, "status": status.HTTP_200_OK})
+
+        # if self._check_if_user_is_not_active(
+        #     serializer.validated_data["username"]
+        # ):
+        #     return Response(
+        #         {
+        #             "error": self.user_inactive_message,
+        #             "status": status.HTTP_403_FORBIDDEN,
+        #         }
+        #     )
+
+        # return Response(
+        #     {
+        #         "error": self.invalid_credentials_message,
+        #         "status": status.HTTP_401_UNAUTHORIZED,
+        #     }
+        # )
+
+        try:
+            user = User.objects.get(username=username)
+
+            if user.check_password(password):
+                if user.is_active:
+                    token = self._create_user_auth_token(user)
+                    return Response(
+                        {"token": token.key, "status": status.HTTP_200_OK},
+                        status=status.HTTP_200_OK,
+                    )
+
+                else:
+                    return Response(
+                        {
+                            "error": self.user_inactive_message,
+                            "status": status.HTTP_403_FORBIDDEN,
+                        },
+                        status=status.HTTP_403_FORBIDDEN,
+                    )
+            raise PermissionDenied
+        except (ObjectDoesNotExist, PermissionDenied):
+            return Response(
+                {
+                    "error": self.invalid_credentials_message,
+                    "status": status.HTTP_401_UNAUTHORIZED,
+                },
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
 
     def _create_user_auth_token(self, user: User) -> str:
         token, _ = Token.objects.get_or_create(user=user)
         return token
 
+    # def _check_if_user_is_not_active(self, username: str) -> bool:
+    #     try:
+    #         user = User.objects.get(username=username)
+    #         if not user.is_active:
+    #             return True
+    #     except ObjectDoesNotExist:
+    #         return False
+
 
 class LogoutView(views.APIView):
     def post(self, request):
         self._delete_user_auth_token(request)
-        return Response({"status": status.HTTP_200_OK})
+        return Response(
+            {"status": status.HTTP_200_OK}, status=status.HTTP_200_OK
+        )
 
     def _delete_user_auth_token(self, request: HttpRequest) -> None:
         Token.objects.filter(user=request.user).delete()
