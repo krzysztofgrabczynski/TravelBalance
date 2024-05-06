127297ec559a4f80e1a6bbdba5f1db6a8434b5f6
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-05-06 22:59:21 +0200

    Delete schema signal for sending activation email after creating user

--- backend/api/user/signals.py
+++ backend/api/user/signals.py
@@ -1,17 +1,7 @@
-from django.dispatch import receiver
+from django.dispatch import receiver, Signal
 from django.contrib.auth.models import User
 from django.db.models import signals
-
-
-@receiver(signals.post_save, sender=User)
-def send_activation_email_post_save(sender, instance, created, **kwargs):
-    """
-    Signal for sending email with activation code for activate user account (admin accounts not included).
-    """
-    if created and not (instance.is_staff or instance.is_superuser):
-        print("Send email to %s" % instance.username)
-    else:
-        print("%s just saved" % instance.username)
+from api.user.email import ActivationEmail
 
 
 @receiver(signals.post_save, sender=User)
