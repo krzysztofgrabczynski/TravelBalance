60b6c2c6e213a883d40011aaa6681843ba4d0495
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-05-06 23:00:17 +0200

    Create 'account_activation' viewset action

--- backend/api/user/views.py
+++ backend/api/user/views.py
@@ -7,6 +7,7 @@ from django.http import HttpRequest
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 
 from api.user import serializers
+from api.user.email import ActivationEmail
 
 
 class LoginView(generics.GenericAPIView):
@@ -60,7 +61,7 @@ class LoginView(generics.GenericAPIView):
 class LogoutView(views.APIView):
     def post(self, request):
         self._delete_user_auth_token(request)
-        return Response({}, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def _delete_user_auth_token(self, request: HttpRequest) -> None:
         Token.objects.filter(user=request.user).delete()
@@ -73,15 +74,31 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "create":
             return serializers.UserCreateSerializer
+        elif self.action == "account_activation":
+            return serializers.AccountActivationSerializer
         elif self.action == "reset_password":
             return ...
 
         return self.serializer_class
 
-    @action(methods=["post"], detail=True)
-    def reset_password(self, request):
-        pass
+    def perform_create(self, serializer):
+        user = serializer.save()
+
+        context = {"user": user}
+        ActivationEmail(self.request, context).send()
+
+        return user
 
     @action(methods=["post"], detail=True)
-    def account_activation(self, request):
+    def account_activation(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=kwargs)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.user
+        user.is_active = True
+        user.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=["post"], detail=True)
+    def reset_password(self, request):
         pass
