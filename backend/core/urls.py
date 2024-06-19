0a350d7009a6dd3567210ed8513fc652135fb3d6
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-06-19 23:26:12 +0200

    Change urls files - delete urls.py files from apps and add new urls.py file in api directory

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from api.user import urls as user_urls
-from api.trip import urls as trip_urls
-from api.expense import urls as expense_urls
+from api import urls as api_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(user_urls)),
-    path("api/", include(trip_urls)),
-    # path("api/", include(expense_urls)),
+    path("api/v1/", include(api_urls)),
 ]
