67bfee055dba4d547f4978cdf99ffd5e21baab15
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-06-19 21:51:01 +0200

    Create serializer for Trip model

---
+++ backend/api/trip/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from api.trip.models import Trip
+
+
+class TripSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Trip
+        fields = ["name", "image"]
