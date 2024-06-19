ae5f95950ab66a76592077f0b6c9d66c0b7b264c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-06-19 21:21:32 +0200

    Add new app to INSTALLED_APPS and to the url paths

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "api.user",
     "api.trip",
+    "api.expense",
 ]
 
 MIDDLEWARE = [
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 
 from api.user import urls as user_urls
 from api.trip import urls as trip_urls
+from api.expense import urls as expense_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(user_urls)),
     path("api/", include(trip_urls)),
+    path("api/", include(expense_urls)),
 ]
