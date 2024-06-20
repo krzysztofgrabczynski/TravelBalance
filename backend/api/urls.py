baaea0835e211669e97accfae3bfbcf8f76d0eb2
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-06-20 15:49:59 +0200

    Add new router for ExpenseViewSet class

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,11 +3,13 @@ from rest_framework.routers import DefaultRouter
 
 from api.user import views as user_views
 from api.trip import views as trip_views
+from api.expense import views as expense_views
 
 
 router = DefaultRouter()
 router.register(r"user", user_views.UserViewSet, basename="user")
 router.register(r"trip", trip_views.TripViewSet, basename="trip")
+router.register(r"expense", expense_views.ExpenseViewSet, basename="expense")
 
 
 urlpatterns = [
