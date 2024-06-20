053339443edd9251b1158fa2415b7492dfd92ff1
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-06-20 15:49:18 +0200

    Create Expense model

--- backend/api/expense/models.py
+++ backend/api/expense/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+from api.trip.models import Trip
+
+
+class Expense(models.Model):
+    class ExpenseCategory(models.IntegerChoices):
+        ACCOMODATION = 0
+        FOOD = 1
+        HEALTH = 2
+        TRANSPORT = 3
+        ACTIVITIES = 4
+        OTHERS = 5
+
+    trip = models.ForeignKey(
+        Trip, on_delete=models.CASCADE, related_name="expenses"
+    )
+    title = models.CharField(max_length=32)
+    cost = models.FloatField(validators=[MinValueValidator(0)])
+    category = models.IntegerField(blank=True, choices=ExpenseCategory.choices)
+    data = models.DateField(default=timezone.now().date())
+
+    @property
+    def get_user(self) -> User:
+        return self.trip.user
+
+    def __str__(self) -> str:
+        return self.title
