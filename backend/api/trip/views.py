efb6e65ce43eb8900832da44e7fd88c70571d856
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-06 23:44:39 +0200

    Add restriction for add trip (limit if the user is not a subscriber)

--- backend/api/trip/views.py
+++ backend/api/trip/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from django.utils.decorators import method_decorator
 
 from api.trip.models import Trip
 from api.trip.serializers import TripSerializer
 from api.permissions import ObjectOwnerPermission
+from api.subscription_restrictions import add_trip_limit
 
 
+@method_decorator(add_trip_limit, name="create")
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
