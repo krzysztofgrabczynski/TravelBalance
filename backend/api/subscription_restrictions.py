64cc3c222a9c36a0240a442b52351edee80b7090
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-06 23:44:04 +0200

    Create file with limitations for the non-subscriber users

---
+++ backend/api/subscription_restrictions.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from django.contrib.auth.models import User
+from rest_framework.exceptions import PermissionDenied
+
+
+def check_if_subscriber(user: User) -> bool:
+    return user.groups.filter(name="subscriber").exists()
+
+def add_trip_limit(func):
+    max_trip_amount = 5
+
+    @wraps(func)
+    def wrapped(request, *args, **kwargs): 
+        user = request.user
+        if not check_if_subscriber(user):
+            if user.trips.count() >= max_trip_amount:
+                raise PermissionDenied
+            
+        return func(request, *args, **kwargs)
+
+    return wrapped
