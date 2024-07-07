4a706de810a50a3cf31865b8714285165acc5a28
BartekG <gajdurbartlomiej@gmail.com>
2024-07-07 17:22:55 +0200

    upd:Expense model added

---
+++ wanderer/lib/models/expense.dart
@@ -0,0 +1,45 @@
+enum Category { accommodation, food, health, transport, activities, others }
+
+class Expense {
+  int? _id;
+  String? _title;
+  double? _cost;
+  Category? _category;
+  DateTime? _dateTime;
+
+  Expense({
+    required int id,
+    required String title,
+    required double cost,
+    required Category category,
+    required DateTime dateTime,
+  })  : _id = id,
+        _title = title,
+        _cost = cost,
+        _category = category,
+        _dateTime = dateTime;
+
+  int? get id => _id;
+  String? get title => _title;
+  double? get cost => _cost;
+  Category? get category => _category;
+  DateTime? get dateTime => _dateTime;
+
+  factory Expense.fromJson(Map<String, dynamic> data) {
+    //Add validations.
+    final int id = data['id'];
+    final String title = data['title'];
+    final double cost = data['cost'];
+    //Might result in problems. Contact Krzysztof abt Categories.
+    final Category category = Category.values[data['category']];
+    final DateTime dateTime = data['data'];
+    return Expense(
+        id: id,
+        title: title,
+        cost: cost,
+        category: category,
+        dateTime: dateTime);
+  }
+
+  //No setters - no editable.
+}
