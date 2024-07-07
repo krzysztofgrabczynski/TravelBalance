02964967502f7cc50da49fbf099843a01960419c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-07 16:25:18 +0200

    Create action for view all user's trips with expenses

--- backend/api/trip/serializers.py
+++ backend/api/trip/serializers.py
@@ -1,12 +1,33 @@
 from rest_framework import serializers
 
 from api.trip.models import Trip
+from api.expense.serializers import ExpenseSerializer
+
 
 
 class TripSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    trip_cost = serializers.SerializerMethodField()
 
     class Meta:
         model = Trip
-        fields = ["id", "user", "name", "image"]
+        fields = ["id", "user", "name", "image", "trip_cost"]
         extra_kwargs = {"id": {"read_only": True}}
+
+    def get_trip_cost(self, obj: Trip) -> float | int:
+        return obj.trip_cost
+
+class TripWithExpensesSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    trip_cost = serializers.SerializerMethodField()
+    expenses = ExpenseSerializer(many=True)
+
+    class Meta:
+        model = Trip
+        fields = ["id", "user", "name", "image", "trip_cost", "expenses"]
+        extra_kwargs = {"id": {"read_only": True}}
+
+    def get_trip_cost(self, obj: Trip) -> float | int:
+            return obj.trip_cost
+
+
--- backend/api/trip/views.py
+++ backend/api/trip/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.utils.decorators import method_decorator
+from rest_framework.decorators import action
 
 from api.trip.models import Trip
-from api.trip.serializers import TripSerializer
+from api.trip.serializers import TripSerializer, TripWithExpensesSerializer
 from api.permissions import ObjectOwnerPermission
 from api.subscription_restrictions import add_trip_limit
 
@@ -16,6 +18,18 @@ class TripViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        if self.action == "list":
+        if self.action == "list" or self.action == "get_trip_with_expenses":
             return queryset.filter(user=self.request.user)
         return queryset
+    
+    def get_serializer_class(self, *args, **kwargs):
+        if self.action == "get_trip_with_expenses":
+            return TripWithExpensesSerializer
+        return self.serializer_class
+
+    @action(methods=["GET"], detail=False)
+    def get_trip_with_expenses(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
