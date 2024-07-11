813cf2666ecf7911cb21a50e93f93b373dce4382
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-11 15:21:37 +0200

    Add trip obj into expens obj when expense obj is saving or modifying

--- backend/api/expense/views.py
+++ backend/api/expense/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from api.expense.models import Expense
+from api.trip.models import Trip
 from api.expense.serializers import ExpenseSerializer
 from api.permissions import TripOwnerPermission, ObjectOwnerPermission
 
@@ -22,3 +23,14 @@ class ExpenseViewSet(viewsets.ModelViewSet):
             trip_pk = self.kwargs.get(self.trip_lookup_field)
             return queryset.filter(trip_id=trip_pk)
         return queryset
+
+    def perform_create(self, serializer):
+        self._perform_save_with_trip_obj(serializer)
+
+    def perform_update(self, serializer):
+        self._perform_save_with_trip_obj(serializer)
+
+    def _perform_save_with_trip_obj(self, serializer):
+        trip_pk = self.kwargs.get(self.trip_lookup_field)
+        trip = Trip.objects.get(pk=trip_pk)
+        serializer.save(trip=trip)
