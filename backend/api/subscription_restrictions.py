0035f48d7015a1314a6c125a08591dfb235a805c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-11 15:20:08 +0200

    Change 'max_trip_amout' for testing

--- backend/api/subscription_restrictions.py
+++ backend/api/subscription_restrictions.py
@@ -6,16 +6,17 @@ from rest_framework.exceptions import PermissionDenied
 def check_if_subscriber(user: User) -> bool:
     return user.groups.filter(name="subscriber").exists()
 
+
 def add_trip_limit(func):
-    max_trip_amount = 5
+    max_trip_amount = 500
 
     @wraps(func)
-    def wrapped(request, *args, **kwargs): 
+    def wrapped(request, *args, **kwargs):
         user = request.user
         if not check_if_subscriber(user):
             if user.trips.count() >= max_trip_amount:
                 raise PermissionDenied
-            
+
         return func(request, *args, **kwargs)
 
     return wrapped
