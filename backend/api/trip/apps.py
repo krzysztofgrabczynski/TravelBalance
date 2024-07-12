3ff0c145fa9aec587a766b41117ab591a531b218
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-07-12 18:51:27 +0200

    Create django signals for trip app - create resize imgae signal for Trip model pre save

--- backend/api/trip/apps.py
+++ backend/api/trip/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TripConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api.trip"
+
+    def ready(self):
+        import api.trip.signals
