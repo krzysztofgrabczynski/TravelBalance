82f0c70fcd3c191834ec6ae8bd41e8a697d0855b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-10-01 16:16:51 +0200

    Update trip modelviewset

--- backend/api/trip/views.py
+++ backend/api/trip/views.py
@@ -7,7 +7,7 @@ from django.utils.decorators import method_decorator
 
 from api.trip.models import Trip
 from api.expense.models import Expense
-from api.trip.serializers import TripSerializer, TripWithExpensesSerializer
+from api.trip.serializers import TripReadSerializer, TripWriteSerializer
 from api.permissions import ObjectOwnerPermission
 from api.subscription_restrictions import add_trip_limit
 
@@ -15,25 +15,41 @@ from api.subscription_restrictions import add_trip_limit
 @method_decorator(add_trip_limit, name="create")
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
-    serializer_class = TripSerializer
+    serializer_class = TripReadSerializer
     permission_classes = [IsAuthenticated, ObjectOwnerPermission]
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        if self.action == "list" or self.action == "get_trips_with_expenses":
+        if self.action == "list":
             return queryset.filter(user=self.request.user)
         return queryset
 
     def get_serializer_class(self, *args, **kwargs):
-        if self.action == "get_trips_with_expenses":
-            return TripWithExpensesSerializer
+        if (
+            self.action == "create"
+            or self.action == "update"
+            or self.action == "partial_update"
+            or self.action == "destroy"
+        ):
+            return TripWriteSerializer
         return self.serializer_class
 
-    @action(methods=["GET"], detail=False)
-    def get_trips_with_expenses(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+
+        user_trips = Trip.objects.filter(user=request.user)
+        total_trips_amount = user_trips.count()
+        visited_countries_amount = sum(
+            [t.countries.count() for t in user_trips]
+        )
+        spendings = sum([c.trip_cost for c in user_trips])
+        extra_content = {
+            "total_trips_amount": total_trips_amount,
+            "visited_countries_amount": visited_countries_amount,
+            "spendings": spendings,
+        }
+
+        return Response({"statistics": extra_content, "trips": response.data})
 
     @action(methods=["GET"], detail=True)
     def get_cost_per_category(self, request, *args, **kwargs):
