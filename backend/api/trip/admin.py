776db8abb92de00cf973f651ccb37b27048eac3a
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-10-02 19:50:09 +0200

    Add new model to admin site - Image model

--- backend/api/trip/admin.py
+++ backend/api/trip/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from api.trip.models import Trip, Country
+from api.trip.models import Trip, Country, Image
 
 
 admin.site.register(Trip)
 admin.site.register(Country)
+admin.site.register(Image)
