d2e9aff36acb64c317a4865daac77cf0008d025f
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-10-22 20:01:22 +0200

    Create more specific admin view for Expense model

--- backend/api/expense/admin.py
+++ backend/api/expense/admin.py
@@ -3,4 +3,28 @@ from django.contrib import admin
 from api.expense.models import Expense
 
 
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "trip_id",
+        "user_id",
+        "username",
+        "title",
+        "cost",
+        "category",
+        "date",
+    )
+    list_filter = ("trip", "category", "date")
+    ordering = ("-date",)
+
+    def trip_id(self, obj: Expense) -> int:
+        return obj.trip.id
+
+    def user_id(self, obj: Expense) -> int:
+        return obj.trip.user.id
+
+    def username(self, obj: Expense) -> str:
+        return obj.trip.user.username
+
+
+admin.site.register(Expense, ExpenseAdmin)
