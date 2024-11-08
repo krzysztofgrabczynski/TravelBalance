861cfb6b117f521b45fa432f8b8874f3438f09bd
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-08 16:48:02 +0100

    Register FeedbackFromUser model on admin site

--- backend/api/user/admin.py
+++ backend/api/user/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from api.user.models import ForgotPasswordToken
+from api.user.models import ForgotPasswordToken, FeedbackFromUser
 
 
 admin.site.register(ForgotPasswordToken)
+
+
+@admin.register(FeedbackFromUser)
+class FeedbackFromUserAdmin(admin.ModelAdmin):
+    list_display = ("user", "type", "message", "created_date", "resolved")
+    list_filter = ("user", "type", "resolved")
+    ordering = ("-created_date",)
