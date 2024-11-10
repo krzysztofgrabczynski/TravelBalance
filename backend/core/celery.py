9a762c8fd3a1e432f80d4c1e199af180d603f232
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-10 23:59:35 +0100

    Register task in celery beat

--- backend/core/celery.py
+++ backend/core/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -9,4 +10,12 @@ app = Celery("core")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(["api.currency"])
+
+app.conf.beat_schedule = {
+    "fetch-currencies-rates-at-midnight": {
+        "task": "save_currencies_rates_task",
+        "schedule": 5.0,  # development only
+        # "schedule": crontab(hour=0, minute=0),
+    }
+}
