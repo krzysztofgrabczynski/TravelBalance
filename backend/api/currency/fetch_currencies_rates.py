0e5b8fabb6e99cb0de35bc4138104adb326c2954
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-11 13:34:20 +0100

    Delete mongodb

--- backend/api/currency/fetch_currencies_rates.py
+++ backend/api/currency/fetch_currencies_rates.py
@@ -1,10 +1,8 @@
 import requests
 from dotenv import load_dotenv
-from datetime import datetime
+
 import os
 from api.currency.models import CurrencyRates
-
-from core.mongodb_connection import MongoDBConnection
 
 
 load_dotenv()
@@ -22,16 +20,3 @@ def save_currencies_rates():
     except:
         pass
         # not implemented
-
-
-def save_currencies_rates_mongodb():
-    with MongoDBConnection() as db:
-        collection = db["currencies_rates"]
-        url = "https://api.apilayer.com/exchangerates_data/latest?base=USD"
-        headers = {"apikey": "i74J7vrR4E1GOvFGowFnLgwWSD0xIef1"}
-        response = requests.request("GET", url, headers=headers)
-        result = response.json()
-
-        final_result = {"date": datetime.today(), "rates": result["rates"]}
-
-        collection.insert_one({str(datetime.today()): final_result})
