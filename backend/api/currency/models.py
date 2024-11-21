3b365c3181636567a5d506d83a7d098d7d0a3c3b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-21 11:43:53 +0100

    Add index to CurrencyRates model

--- backend/api/currency/models.py
+++ backend/api/currency/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class CurrencyRates(models.Model):
     date = models.DateField(auto_now_add=True)
     rates = models.JSONField()
+
+    class Meta:
+        indexes = [models.Index(fields=["-date"])]
