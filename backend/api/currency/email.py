887e63b68b0a917d12efaa71e3b6ca231d05811c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-25 14:45:48 +0100

    Code refactor in FetchCurrencyFailedEmail

--- backend/api/currency/email.py
+++ backend/api/currency/email.py
@@ -6,10 +6,3 @@ from api.user.email import BaseEmailMessage
 class FetchCurrencyFailedEmail(BaseEmailMessage):
     template_name = settings.FETCH_CURRENCY_FAILED_EMAIL_TEMPLATE
     email_subject = settings.FETCH_CURRENCY_FAILED_EMAIL_SUBJECT
-
-    def set_context_data(self) -> None:
-        super().set_context_data()
-
-        status_code = self.context["status_code"]
-        message = self.context["message"]
-        self.context.update({"status_code": status_code, "message": message})
