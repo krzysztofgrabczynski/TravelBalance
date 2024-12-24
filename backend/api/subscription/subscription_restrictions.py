a96806793dc427749b0488f8189bd3cb8a9898c4
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-12-24 14:20:57 +0100

    Create new app for manage subscription

---
+++ backend/api/subscription/subscription_restrictions.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from django.contrib.auth import get_user_model
+from api.user.models import MyUser
+from rest_framework.exceptions import PermissionDenied
+
+
+User = get_user_model()
+
+
+def check_if_subscriber(user: MyUser) -> bool:
+    return user.is_premium
+
+
+def add_trip_limit(func):
+    max_trip_amount = 500
+
+    @wraps(func)
+    def wrapped(request, *args, **kwargs):
+        user = request.user
+        if not check_if_subscriber(user):
+            if user.trips.count() >= max_trip_amount:
+                raise PermissionDenied
+
+        return func(request, *args, **kwargs)
+
+    return wrapped
