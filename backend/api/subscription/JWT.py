e956ac4eef7b9a7686f884d3ab45b451d546ac10
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-12-28 12:19:56 +0100

    Change location of the pem file in generate_app_store_server_jwt function

--- backend/api/subscription/JWT.py
+++ backend/api/subscription/JWT.py
@@ -9,11 +9,7 @@ load_dotenv()
 
 
 def generate_app_store_server_jwt() -> str:
-    base_dir = os.path.dirname(__file__)
-    private_key_path = os.path.join(
-        base_dir, os.environ.get("IOS_IAP_PRIVATE_KEY_PATH")
-    )
-    with open(private_key_path, "r") as file:
+    with open(os.environ.get("IOS_IAP_PRIVATE_KEY_PATH"), "r") as file:
         private_key = file.read()
 
     headers = {"kid": os.environ.get("IOS_IAP_KID")}
